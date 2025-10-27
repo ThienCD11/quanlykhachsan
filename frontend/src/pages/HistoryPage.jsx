0dabd7db036bb9a5d4d89b7e9bd9e98c01a7c7d3
ThienCD <caothien110104@gmail.com>
2025-10-27 11:29:48 +0700

    ok

---
+++ frontend/src/pages/HistoryPage.jsx
@@ -0,0 +1,19 @@
+import React, { useState } from "react";
+import axios from "axios";
+import Header from "../components/Header";
+import Footer from "../components/Footer";
+
+const HistoryPage = () => {
+
+  return (
+    <div>
+      <Header />
+
+      <div style={{margin: '500px'}}> Hello congthuc</div>
+
+      <Footer />
+    </div>
+  );
+};
+
+export default HistoryPage;
